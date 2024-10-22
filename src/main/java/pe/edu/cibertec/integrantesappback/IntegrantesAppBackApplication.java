7fcf97cd776d5f75c3996c9f82189d3594572026
Iann <falconcolos@gmail.com>
2024-10-22 02:52:54 -0500

    Creacion del proyecto

---
+++ src/main/java/pe/edu/cibertec/integrantesappback/IntegrantesAppBackApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.integrantesappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrantesAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrantesAppBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/integrantesappback/IntegrantesAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.integrantesappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrantesAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
